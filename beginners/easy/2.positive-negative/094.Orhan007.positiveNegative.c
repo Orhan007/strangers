93b85bc1d3bd9b28e548f3d9cfc237fe589b6b42
Orhan007 <101088241+Orhan007@users.noreply.github.com>
2022-04-03 14:54:42 +0600

    Add files via upload

---
+++ beginners/easy/2.positive-negative/094.Orhan007.positiveNegative.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a>=0)
+        printf("This is a positive number.");
+    else
+        printf("This is a negative number.");
+}
